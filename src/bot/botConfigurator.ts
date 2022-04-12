5d69288925a7c989ab122c4a30b2d81202046cdd
Michael Uskov <uskovm@gmail.com>
2022-04-12 12:55:29 +0500

    Adding users

--- src/bot/botConfigurator.ts
+++ src/bot/botConfigurator.ts
@@ -1,30 +1,67 @@
-import { Context, Telegraf } from "telegraf";
-const { MongoClient } = require("mongodb");
+import { Context, Scenes, Telegraf } from "telegraf";
+import { MongoClient } from "mongodb";
 const { session } = require("telegraf-session-mongodb");
 import { injectable } from "tsyringe";
 
 export async function getDatabase() {
-  const client = new MongoClient(process.env.MONGO_URL);
+  const client = new MongoClient(process.env.MONGO_URL || "");
   await client.connect();
   return client.db();
 }
 
-export interface LinguaBotContext extends Context {
+export interface PhotoGameBotContext extends Context {
+  myContextProp: string;
+
+  // declare scene type
+  scene: Scenes.SceneContextScene<PhotoGameBotContext>;
 }
 
 @injectable()
 export class BotConfigurator {
   isConfigured: boolean = false;
 
-  async configureBot(bot: Telegraf<LinguaBotContext>) {
+  async configureBot(bot: Telegraf<PhotoGameBotContext>) {
     this.isConfigured = true;
 
     const database = await getDatabase();
-    bot.use(session(database, { collectionName: 'sessions' }));
+    bot.use(session(database, { collectionName: "sessions" }));
 
-    
-    bot.start((ctx) => ctx.reply("ЖОПА"));
-    bot.hears(/.*/, (ctx) => ctx.reply("ЧЕ?"));
+    const { enter, leave } = Scenes.Stage;
+
+    // Greeter scene
+    const greeterScene = new Scenes.BaseScene<PhotoGameBotContext>("greeter");
+    greeterScene.enter((ctx) => ctx.reply("Для начала давай представимся. Напиши, как тебя зовут"));
+    greeterScene.on("message", async (ctx) => {
+      const name = (ctx.message as any).text;
+      await database.collection("users").insertOne({
+        telegram_id: ctx.message.from.id,
+        name
+      });
+      await ctx.reply(`Хорошо, ${name}. Приятно познакомиться!`);
+      await ctx.scene.leave();
+    });
+
+    const stage = new Scenes.Stage<PhotoGameBotContext>([greeterScene], {
+      ttl: 10,
+    });
+    bot.use(stage.middleware());
+    bot.use((ctx, next) => {
+      // we now have access to the the fields defined above
+      ctx.myContextProp ??= "";
+      return next();
+    });
+    bot.command("greeter", (ctx) => ctx.scene.enter("greeter"));
+    bot.on("message", async (ctx) => {
+      const database = await getDatabase();
+      const user = await database.collection("users").findOne<any>({
+        telegram_id: ctx.message.from.id
+      });
+      if (!user) {
+        ctx.scene.enter("greeter");
+      } else {
+        ctx.reply(`ПРИВЕТ, ${user.name}`);
+      }
+    });
 
     return bot;
   }
